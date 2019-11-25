977ec482fb8f912ae653508de9950b93dfa6740b
Student <student@d17-l427-pc-19.ipa.ki.agh.edu.pl>
2019-11-25 09:33:32 +0100

    blabla

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="iAmClicked()">Click me!</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {title: 'Clicked 0 times'}
+    },
+    methods: {
+      iAmClicked() {
+        this.title= 'Clicked';
+      }
+
+    }
+  };
+</script>
